b37e89c81ebf6f0cea9bd116b93a02f750037dc3
Alexandre <alexandrefaria3012@gmail.com>
2023-06-27 17:43:44 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/pages/listaDeTarefas.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,4 +12,5 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
+  }
 }
