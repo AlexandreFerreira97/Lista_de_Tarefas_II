a59b655e404e3edcfdf4338d9139c8e4486f9f40
Alexandre <alexandrefaria3012@gmail.com>
2023-06-29 10:53:54 -0300

    Update listaDeTarefas.dart

--- lib/pages/listaDeTarefas.dart
+++ lib/pages/listaDeTarefas.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:async';
@@ -11,6 +13,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
@@ -19,6 +24,21 @@ class _HomeState extends State<Home> {
   Future <File> _getFile() async{
     final directory = await getApplicationDocumentsDirectory();
     return File ('${directory.path}/data.json');
+  }
+
+  Future <File> _saveData() async{
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future <String?> _readData() async{
+    try{
+      final file = await _getFile();
+      return file.readAsString();
+    }catch(e){
+      return null;
+    }
   }
 }
 
